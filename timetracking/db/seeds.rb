bd0c18cd4ffb4518c19ead2fe4f51b1f70a78fb8
leucha <leucha@gmail.com>
2016-04-19 14:01:27 -0400

    finished learning about dbs for the day

--- timetracking/db/seeds.rb
+++ timetracking/db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-tacos = [{name: 'Ironhack', description: "Crazy Little Course down in miami"}, {name: 'Tacos', description: "Best food in the world that i need"}, {name: 'Shopping', description: "Get some new clothes for the emporer"}, {name: 'Bike Ride', description: "All the mocha lattes you gotta do pilates"}]
+tacos = [{name: 'Ironhack', description: "Crazy Little Course down in miami"}, {name: 'Tacos', description: "Best food in the world that i need"}, {name: 'Shopping', description: "Get some new clothes for the emporer"}, {name: 'Bike Ride', description: "All the mocha lattes you gotta do pilates"}, {name: 'Read Catcher in the Rye', description: "Book that will really help me rethink my life"}, {name: 'Kill The Phonies', description: "Start with John Lennon"}, {name: 'Bodegon', description: "Pick up some spanish chicks"}]
 projects = Project.create(tacos)
