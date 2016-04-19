f39960c889a3c18edcefccad2edefc67fa1ab2da
leucha <leucha@gmail.com>
2016-04-19 13:53:27 -0400

    Deleted Moviequiz and am tweaking the db

--- timetracking/db/seeds.rb
+++ timetracking/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+tacos = [{name: 'Ironhack', description: "Crazy Little Course down in miami"}, {name: 'Tacos', description: "Best food in the world that i need"}, {name: 'Shopping', description: "Get some new clothes for the emporer"}, {name: 'Bike Ride', description: "All the mocha lattes you gotta do pilates"}]
+projects = Project.create(tacos)
