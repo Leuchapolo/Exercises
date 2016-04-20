8de0eea8be5d6ceb873d275ace2ab5e79400b2c1
leucha <leucha@gmail.com>
2016-04-20 10:16:12 -0400

    Made a couple changes to time tracking and deleted word_central

--- timetracking/app/controllers/projects_controller.rb
+++ timetracking/app/controllers/projects_controller.rb
@@ -3,4 +3,15 @@ class ProjectsController < ApplicationController
 		@projects = Project.order(created_at: :desc).limit(10)
 		render 'index'
 	end
+
+	def show 
+		@project = Project.find_by(id: params[:id])
+
+		if @project != nil
+			render 'show'
+		else
+			render "no_projects_found"
+		end
+		
+	end
 end
--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -2,15 +2,9 @@ Rails.application.routes.draw do
 
 	get '/', to: 'site#home'
 
-  get '/contact', to: 'site#contact'
-
-  get '/say_name/:name', to: 'site#say_name'
-
-  get '/calculator', to: 'site#calculator'
-
-  post '/calculate', to: 'site#calculate'
-
   get '/projects', to: 'projects#index'
+
+  get '/projects/show/:id', to: 'projects#show'
   
 
   # The priority is based upon order of creation: first created -> highest priority.
